4b41ccba4541e7ef8fb141053871dc9b125dd2d9
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-02-19 12:34:24 -0600

    V.1.0 Teléfono

--- assets/js/animaciones.js
+++ assets/js/animaciones.js
@@ -1,3 +1,4 @@
+
 document.querySelector(".bars-menu").addEventListener("click", animateBars);
 
 var line1_bar = document.querySelector(".bars-menu__line1");
@@ -10,5 +11,17 @@ function animateBars(){
     line2_bar.classList.toggle("bars-menu__line2--active");
     line3_bar.classList.toggle("bars-menu__line3--active");
 
-    menu.classList.toggle("menu--active")
+    menu.classList.toggle("menu--active");
 }
+
+
+
+//const label1 =  document.querySelector("#label1");
+//const grupo1 =  document.getElementById("#grupo1");
+//const input1 =  document.getElementById("#input1");
+
+//input1.addEventListener("focus", animateLabel1);
+
+//function animateLabel1(){
+//    grupo1.classList.toggle("focus");
+//}
